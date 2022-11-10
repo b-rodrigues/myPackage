21b5a9a81c2997489bc09a439cb7e2caca5670de
Bruno Rodrigues <bruno@brodrigues.co>
2022-11-10 08:57:22 +0100

    added 2016 to data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,6 +3,7 @@
 unemp_2013 <- readr::read_csv("https://raw.githubusercontent.com/b-rodrigues/modern_R/master/datasets/unemployment/unemp_2013.csv")
 unemp_2014 <- readr::read_csv("https://raw.githubusercontent.com/b-rodrigues/modern_R/master/datasets/unemployment/unemp_2014.csv")
 unemp_2015 <- readr::read_csv("https://raw.githubusercontent.com/b-rodrigues/modern_R/master/datasets/unemployment/unemp_2015.csv")
+unemp_2016 <- readr::read_csv("https://raw.githubusercontent.com/b-rodrigues/modern_R/master/datasets/unemployment/unemp_2016.csv")
 
 library(dplyr)
 
@@ -31,7 +32,8 @@ clean_data <- function(x){
 my_datasets <- list(
   unemp_2013,
   unemp_2014,
-  unemp_2015
+  unemp_2015,
+  unemp_2016
 )
 
 unemp <- purrr::map_dfr(my_datasets, clean_data)
